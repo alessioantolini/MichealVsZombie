d6a4576f46af9f2e7d31242451fa51707444f378
Alessio Antolini <alessio@alessioantolini.it>
2021-03-14 14:17:35 +0100

    Collision phase1

--- MichealNinjaState.gd
+++ MichealNinjaState.gd
@@ -16,6 +16,7 @@ func get_input():
 	var _direction: = Input.get_action_strength("move_right") - Input.get_action_strength("move_left")
 	var _isJump: = -1.0 if (Input.get_action_strength("move_jump") and parent.is_on_floor()) else 1.0	
 	
+	#Attacco
 	_attack = Input.get_action_strength("move_attack")
 	
 	parent.direction = Vector2 (
